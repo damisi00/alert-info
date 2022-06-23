ae8bb7079af5be08a09dd0492f3f568bf1cd04a1
damisi00 <80995124+damisi00@users.noreply.github.com>
2022-06-23 01:39:01 +0100

    Add files via upload

---
+++ info.js
@@ -0,0 +1,7 @@
+//program that prints name, height, and country
+
+let myName = "Oluwadamisi Enioluwafe";
+let height = 5.6;
+let country = "Nigeria";
+
+alert(`My name is ${myName}, I'm about ${height}ft tall and ${country} is my country.`);
